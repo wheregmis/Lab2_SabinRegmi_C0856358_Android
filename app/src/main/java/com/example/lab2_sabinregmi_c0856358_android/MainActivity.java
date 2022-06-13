b1041ad3697646993ed9b8c084cf230d7eda5da3
Sabin Regmi <xawbeenregmi@gmail.com>
2022-06-13 17:24:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab2_sabinregmi_c0856358_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab2_sabinregmi_c0856358_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
