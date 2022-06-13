a487370eeb473093f0936105d2692dde446ccd82
Sabin Regmi <xawbeenregmi@gmail.com>
2022-06-13 17:34:17 -0400

    Added Products Model and Database Helper
    -- added product model and database helper

---
+++ app/src/main/java/com/example/lab2_sabinregmi_c0856358_android/model/Product.java
@@ -0,0 +1,32 @@
+package com.example.lab2_sabinregmi_c0856358_android.model;
+
+public class Product {
+
+    int id;
+    String name, description;
+    double price;
+
+    public Product(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
---
+++ app/src/main/java/com/example/lab2_sabinregmi_c0856358_android/utils/DatabaseHelper.java
@@ -0,0 +1,116 @@
+package com.example.lab2_sabinregmi_c0856358_android.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "product_database";
+
+    private static final String TABLE_NAME = "product";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESC = "description";
+    private static final String COLUMN_PRICE = "price";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER NOT NULL CONSTRAINT employee_pk PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " VARCHAR(20) NOT NULL, " +
+                COLUMN_DESC + " VARCHAR(20) NOT NULL, " +
+                COLUMN_PRICE + " DOUBLE NOT NULL);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // drop table and then create it
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+    // insert
+    /**
+     * add employee - insert employee into employee table
+     * @param name
+     * @param description
+     * @param price
+     * @return boolean value - true (inserted) false (not inserted)
+     * */
+    public boolean addEmployee(String name, String description, double price) {
+        // we need a writeable instance of SQLite database
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        // we need to define a content values in order to insert data into our database
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, name);
+        contentValues.put(COLUMN_DESC, description);
+        contentValues.put(COLUMN_PRICE, String.valueOf(price));
+
+        // the insert method associated to SQLite database instance returns -1 if nothing is inserted
+        return sqLiteDatabase.insert(TABLE_NAME, null, contentValues) != -1;
+    }
+
+    /**
+     * Query database - select all the employees
+     * @return cursor
+     * */
+    public Cursor getAllEmployees() {
+        // we need a readable instance of database
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        return sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+
+    /**
+     * Update employee in database
+     * @param id
+     * @param name
+     * @param description
+     * @param price
+     * @return boolean value - true (successful)
+     * */
+    public boolean updateEmployee(int id, String name, String description, double price) {
+        // we need a writeable instance of database
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, name);
+        contentValues.put(COLUMN_DESC, description);
+        contentValues.put(COLUMN_PRICE, String.valueOf(price));
+
+        // update method associated to SQLite database instance returns number of rows affected
+        return sqLiteDatabase.update(TABLE_NAME,
+                contentValues,
+                COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)}) > 0;
+    }
+
+    /**
+     * Delete employee from database table
+     * @param id
+     * @return true if is successful
+     * */
+    public boolean deleteEmployee(int id) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        // the delete method associated to the SQLite database instance returns the number of rows affected
+        return sqLiteDatabase.delete(TABLE_NAME,
+                COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)}) > 0;
+    }
+
+
+
+
+
+}
